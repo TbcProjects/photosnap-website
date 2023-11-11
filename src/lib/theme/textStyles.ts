68b92f65596dd267a30f3665a7328783b658fb87
TbcProjects <daniel@thebikeclub.co.uk>
2023-11-11 11:30:09 +0000

    Add: textStyles

--- src/lib/theme/textStyles.ts
+++ src/lib/theme/textStyles.ts
@@ -1,11 +1,43 @@
 import { defineTextStyles, TextStyles } from "@pandacss/dev";
 
 export const textStyles: TextStyles = defineTextStyles({
-  // format: textStyleName: {value: {...styles}}
   h1: {
     value: {
-      fontWeight: "bold",
-      fontSize: "45px",
+      fontWeight: "700",
+      fontSize: "40px",
+      lineHeight: "48px",
+      letterSpacing: "4.17px",
+    },
+  },
+  h2: {
+    value: {
+      fontWeight: "700",
+      fontSize: "24px",
+      lineHeight: "25px",
+      letterSpacing: "4.17px",
+    },
+  },
+  h3: {
+    value: {
+      fontWeight: "700",
+      fontSize: "18px",
+      lineHeight: "25px",
+      letterSpacing: "4.17px",
+    },
+  },
+  h4: {
+    value: {
+      fontWeight: "700",
+      fontSize: "12px",
+      lineHeight: "15.62px",
+      letterSpacing: "2px",
+    },
+  },
+  body: {
+    value: {
+      fontWeight: "400",
+      fontSize: "15px",
+      lineHeight: "25px",
     },
   },
 });
