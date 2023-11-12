1403fd16bf902303a10011246d97ab910ad0e355
TbcProjects <daniel@thebikeclub.co.uk>
2023-11-12 14:34:05 +0000

    Update: Pass config variables to DirectionArrow

--- src/components/icons/DirectionalArrow.tsx
+++ src/components/icons/DirectionalArrow.tsx
@@ -6,11 +6,17 @@ import "../../../public/assets/shared/desktop/arrow.svg";
  * - make width/height a prop
  */
 
-function DirectionalArrow() {
+interface DirectionalArrowProps {
+  color: string;
+  width: string;
+  height: string;
+}
+
+function DirectionalArrow({ color, width, height }: DirectionalArrowProps) {
   return (
     <div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="43" height="14">
-        <g fill="none" fill-rule="evenodd" stroke="#000">
+      <svg xmlns="http://www.w3.org/2000/svg" width={width} height={height}>
+        <g fill="none" fill-rule="evenodd" stroke={color}>
           <path d="M0 7h41.864M35.428 1l6 6-6 6" />
         </g>
       </svg>
