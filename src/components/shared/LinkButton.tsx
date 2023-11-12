02db57dbd1e0d3e1ff5197fd49593bbfd559f40f
TbcProjects <daniel@thebikeclub.co.uk>
2023-11-12 14:15:41 +0000

    Create LinkButton Component

---
+++ src/components/shared/LinkButton.tsx
@@ -0,0 +1,35 @@
+import Link, { LinkProps } from "next/link";
+
+import DirectionalArrow from "@components/icons/DirectionalArrow";
+
+import { flex } from "styled-system/patterns";
+import { button, type ButtonVariants } from "@lib/theme/components/buttons";
+import { ReactNode } from "react";
+
+type ButtonProps = LinkProps & ButtonVariants & { children: ReactNode };
+
+const Button = ({ href, prefetch, visual, type, children }: ButtonProps) => {
+  const ButtonVariant = (VariantType: typeof type) => {
+    switch (VariantType) {
+      case "primary":
+        return <button className={button({ visual, type })}>{children}</button>;
+      case "secondary":
+        return (
+          <button className={button({ visual, type })}>
+            <div className={flex({ justify: "space-between" })}>
+              {children}
+              <DirectionalArrow />
+            </div>
+          </button>
+        );
+    }
+  };
+
+  return (
+    <Link href={href} prefetch={prefetch}>
+      {ButtonVariant(type)}
+    </Link>
+  );
+};
+
+export default Button;
